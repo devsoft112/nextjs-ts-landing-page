cdd10fecf1ce093f6d86cf0309d400aafc83d130
devsoft112 <devsoftswe.0205@gmail.com>
2024-10-25 14:24:28 +0300

    fix(howitworks): updated the UI for stepper

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,75 @@
+// src/components/VerticalStepper.tsx
+
+import React from 'react';
+
+const steps = [
+  {
+    id: 1,
+    title: 'Step 1: Register and Create Your Profile',
+    description: `Download the RUSH Gigs app and register using your phone. Complete your profile by providing information about your skills, work experience, and contact details to increase your chances of successfully finding jobs or workers.`,
+  },
+  {
+    id: 2,
+    title: 'Step 2: Search for Opportunities or Candidates',
+    description: `Use the search feature to find job openings or potential workers in your area. Apply filters to refine your results based on specialization, distance, or type of employment. Save interesting job listings or profiles for quick access later.`,
+  },
+  {
+    id: 3,
+    title: 'Step 3: Connect and Start Collaborating',
+    description: `Once you find a suitable opportunity or candidate, use the built-in chat feature to reach out to the employer or worker. Discuss collaboration details, work conditions, and agree on next steps. With RUSH Gigs, it’s simple – your new opportunity is just a click away!`,
+  },
+];
+
+export default function VerticalStepper({
+  activeStep,
+  onStepChange,
+}: {
+  activeStep: number;
+  onStepChange: (stepId: number) => void;
+}) {
+  return (
+    <div className='flex items-start py-8'>
+      {/* Start of the Stepper */}
+      <div className='relative flex flex-col items-center'>
+        {/* Start Connector */}
+        <div className='w-full flex flex-row justify-between'>
+          <div className='w-[2px] h-10 bg-gradient-to-b from-transparent to-blue-300 mb-3 flex-start ml-6'></div>
+        </div>
+
+        {steps.map((step, index) => (
+          <div className='flex flex-row' key={index}>
+            <div className='flex flex-col items-center'>
+              {/* Step Circle */}
+              <div
+                onClick={() => onStepChange(step.id)} // Trigger step change
+                className={`cursor-pointer flex items-center justify-center w-12 h-12 rounded-full text-white font-bold text-xl mb-4 ${
+                  activeStep === step.id
+                    ? 'bg-gradient-to-r from-purple-500 to-blue-500' // Active step style
+                    : 'bg-white text-blue-500 border-2 border-gray-300'
+                }`}
+              >
+                {step.id}
+              </div>
+
+              {/* Connector below the circle (except for the last step) */}
+              {index !== steps.length - 1 && (
+                <div className='w-[2px] h-24 bg-gradient-to-t from-transparent via-blue-300 to-transparent'></div>
+              )}
+            </div>
+
+            <div className='ml-8 mt-2 mb-10'>
+              <h3
+                className={`text-2xl font-semibold mb-2 ${
+                  activeStep === step.id ? 'text-blue-600' : 'text-gray-700'
+                }`}
+              >
+                {step.title}
+              </h3>
+              <p className='text-gray-500'>{step.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/sections/apps/HowItWorks.tsx
+++ src/sections/apps/HowItWorks.tsx
@@ -1,80 +1,98 @@
+// src/sections/HowItWorks.tsx
+
 import Image from 'next/image';
 import React, { useState } from 'react';
 
 import DownloadButton from '@/components/DownloadButton';
+import VerticalStepper from '@/components/Stepper';
 
 export default function HowItWorks() {
   const [selectedStep, setSelectedStep] = useState(steps[0].id);
+  const [isImageLoading, setIsImageLoading] = useState(true); // Manage loading state
+
+  const handleStepChange = (stepId: number) => {
+    setSelectedStep(stepId);
+    setIsImageLoading(true); // Set loading to true when step changes
+  };
+
+  const handleImageLoad = () => {
+    setIsImageLoading(false); // Set loading to false once the image is fully loaded
+  };
 
   return (
     <section className='py-16 px-2 bg-[#F2F5FB]'>
       <div className='container mx-auto text-center'>
         {/* Section Title */}
         <p className='text-orange-500 font-semibold'>How It Works</p>
-        <h3 className='text-4xl font-bold mt-4'>
-          How Does <span className='text-black'>RUSH Gigs</span>{' '}
-          <span className='text-blue-600'>Work ?</span>
+        <h3 className='text-4xl md:text-5xl font-light mt-4 max-w-lg mx-auto'>
+          <span className='font-extrabold'>How</span> Does{' '}
+          <span className='text-blue-600 font-extrabold'>RUSH Gigs</span>{' '}
+          <span className='font-extrabold'>Work ?</span>
         </h3>
       </div>
 
-      <div className='container flex mx-auto py-12'>
-        <div className='flex flex-col md:flex-row md:items-start w-full lg:w-1/2'>
-          <div className='flex flex-col'>
-            {steps.map((step, index) => (
-              <div
-                key={step.id}
-                className='flex items-start mb-8 cursor-pointer'
-                onClick={() => setSelectedStep(step.id)}
-              >
-                <div
-                  className={`p-4 flex items-center justify-center rounded-full border-2 ${
-                    selectedStep === step.id
-                      ? 'bg-blue-600 text-white'
-                      : 'bg-gray-200 text-gray-600'
-                  }`}
-                >
-                  {index + 1}
-                </div>
-                <div className='ml-4'>
-                  <h3
-                    className={`text-lg font-semibold ${
-                      selectedStep === step.id
-                        ? 'text-blue-600'
-                        : 'text-gray-700'
-                    }`}
-                  >
-                    {step.title}
-                  </h3>
-                  {selectedStep === step.id && (
-                    <p className='mt-2 text-gray-500'>{step.description}</p>
-                  )}
-                </div>
+      <div className='container flex flex-col md:flex-row mx-auto py-48 items-center'>
+        {/* Right - Image */}
+        <div className='flex flex-col items-center w-full md:w-3/5 mb-16 md:mb-0'>
+          <div
+            className='relative w-full h-[400px] bg-gray-100 flex items-center justify-center'
+            style={{ minHeight: '400px' }} // Ensure minimum height during image loading
+          >
+            {isImageLoading && (
+              <div className='absolute inset-0 flex items-center justify-center'>
+                {/* Placeholder or loading animation */}
+                <Image
+                  src='/images/loading_mock.png' // mock image or loading skeleton
+                  alt='Loading...'
+                  width={400}
+                  height={200}
+                  className='object-contain'
+                />
               </div>
-            ))}
+            )}
+
+            {selectedStep === 1 && (
+              <Image
+                src='/images/apps_step1.png'
+                alt='Step 1 Image'
+                width={1000}
+                height={500}
+                className='object-contain'
+                onLoadingComplete={handleImageLoad} // Fire once image is loaded
+              />
+            )}
+            {selectedStep === 2 && (
+              <Image
+                src='/images/apps_step2.png'
+                alt='Step 2 Image'
+                width={1000}
+                height={500}
+                className='object-contain'
+                onLoadingComplete={handleImageLoad}
+              />
+            )}
+            {selectedStep === 3 && (
+              <Image
+                src='/images/apps_step3.png'
+                alt='Step 3 Image'
+                width={1000}
+                height={500}
+                className='object-contain'
+                onLoadingComplete={handleImageLoad}
+              />
+            )}
           </div>
         </div>
 
-        <div className='flex flex-col items-center space-y-8 w-full lg:w-1/2'>
-          <Image
-            src='/images/apps_step1.png'
-            alt='Step 1'
-            width={400}
-            height={500}
+        <div className='w-full md:w-2/5'>
+          <VerticalStepper
+            activeStep={selectedStep}
+            onStepChange={handleStepChange}
           />
-          {/* <Image
-              src='/images/apps_step2.png'
-              alt='Step 2'
-              width={400}
-              height={500}
-            />
-            <Image
-              src='/images/apps_step3.png'
-              alt='Step 3'
-              width={400}
-              height={500}
-            /> */}
         </div>
       </div>
+
+      {/* Download Buttons */}
       <div className='mt-8 flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-8'>
         <DownloadButton
           platform='App Store'
